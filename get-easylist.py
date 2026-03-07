5bcc24b3df019f973df550337c99ea34bc9642c8
pcWRT Router <drxi2009@gmail.com>
2026-03-07 14:59:47 -0600

    Update get-easylist.py

--- get-easylist.py
+++ get-easylist.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 import re
+import gzip
+import shutil
 import subprocess
 
 subprocess.run(["curl", "-o", "easylist.txt", "https://easylist.to/easylist/easylist.txt"])
 
-o = open("easylist-domains.txt", "w")
-
+o = open("dist/easylist-domains.txt", "w")
 with open("easylist.txt", "r") as f:
     for line in f:
         m = re.match(r'^\|\|(\S*)\^\s*$', line)
         if m and m.group(1) != None:
             print(m.group(1), file=o)
-
 o.close()
+
+with open('dist/easylist-domains.txt', 'rb') as f_in:
+    with gzip.open('dist/easylist-domains.txt.gz', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+subprocess.run("md5sum dist/easylist-domains.txt.gz | cut -d ' ' -f 1 > dist/easylist-domains.txt.md5", shell=True)
