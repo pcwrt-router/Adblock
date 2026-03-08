631a730dab2bea1c03e38be51a3c2f3c077b8198
pcWRT Router <drxi2009@gmail.com>
2026-03-07 18:15:12 -0600

    Update get-easylist.py

--- get-easylist.py
+++ get-easylist.py
@@ -4,10 +4,10 @@ import gzip
 import shutil
 import subprocess
 
-subprocess.run(["curl", "-o", "easylist.txt", "https://easylist.to/easylist/easylist.txt"])
+subprocess.run(["curl", "-o", "dist/easylist.txt", "https://easylist.to/easylist/easylist.txt"])
 
 o = open("dist/easylist-domains.txt", "w")
-with open("easylist.txt", "r") as f:
+with open("dist/easylist.txt", "r") as f:
     for line in f:
         m = re.match(r'^\|\|(\S*)\^\s*$', line)
         if m and m.group(1) != None:
